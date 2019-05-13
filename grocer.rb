4078f5feccef55f196dfc167d2448d96d90da64e
Tanuka Das <das.tanuka@yahoo.com>
2019-05-13 00:57:28 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,33 @@
+#The cart starts as an array of individual items. Translate it into a hash that includes the
+#counts for each item with the consolidate_cart method.
 def consolidate_cart(cart)
   # code here
+  new_hash = {}
+  cart.each do |items|
+    items.each do |name, attributes|
+      if new_hash[name]
+        new_hash[name][:count] += 1                 #adds a count of one to each item when there are no duplicates
+      else
+        new_hash[name] = attributes
+        new_hash[name][:count] = 1
+      end
+    end
+  end
+  new_hash
 end
+
 
 def apply_coupons(cart, coupons)
   # code here
+  new_hash ={}
+  cart.each do |property, attributes|
+    coupons.each do |coupon|
+      if property = coupon[:item]
+
+    end
+
+  end
+  hash[:item_name] = {"coupon"}
 end
 
 def apply_clearance(cart)
